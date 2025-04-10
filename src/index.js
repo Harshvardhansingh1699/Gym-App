014915c0d43f8d61adc2846ec7365aafed84f9f2
harshvardhansingh1699 <harshvardhnasingh68244@gmail.com>
2025-04-10 18:01:48 +0530

    fixed linting errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-// import { createRoot } from "react-dom/client";
-
-// const root = createRoot(document.getElementById("root"));
-
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
